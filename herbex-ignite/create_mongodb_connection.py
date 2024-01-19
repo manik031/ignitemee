9371f59f3ee4e0d96ee56f0b4a91a5a12d72d851
Manik chopra <129728730+manik031@users.noreply.github.com>
2024-01-20 01:05:12 +0530

    Delete herbex-ignite directory

--- herbex-ignite/create_mongodb_connection.py
+++
@@ -1,8 +0,0 @@
-
-from pymongo import MongoClient
-
-def create_connection_to_mongodb(connection_string):
-    print('conection ding')
-    
-    client = MongoClient(connection_string)
-    return client
